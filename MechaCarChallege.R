78ed5036e068826c1af03302e83bc6f32e2e8b82
mitchell-alexleigh <75285458+mitchell-alexleigh@users.noreply.github.com>
2021-04-11 20:07:35 -0500

    Update MechaCarChallege.R

--- MechaCarChallege.R
+++ MechaCarChallege.R
@@ -1,3 +1,4 @@
+#Deliverable 1
 #load dplyr package
 library(dplyr)
 
@@ -9,3 +10,13 @@ model <- lm(mpg ~ vehicle_length + vehicle_weight + vehicle_weight + ground_clea
 
 #summary
 summary(model)
+
+
+#Deliverable 2
+suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#create total summary dataframe
+tortal_summary <- suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Veriance=var(PSI),SD=sd(PSI))
+
+#create lot summary dataframe
+lot_summary <- suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Veriance=var(PSI),SD=sd(PSI), .groups = 'keep') 
