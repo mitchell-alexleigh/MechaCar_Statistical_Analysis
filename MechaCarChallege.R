2827b5c1d057bba7f88b444df45bdbbcfe94715d
mitchell-alexleigh <75285458+mitchell-alexleigh@users.noreply.github.com>
2021-04-11 21:29:51 -0500

    Update MechaCarChallege.R

--- MechaCarChallege.R
+++ MechaCarChallege.R
@@ -20,3 +20,19 @@ tortal_summary <- suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI
 
 #create lot summary dataframe
 lot_summary <- suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Veriance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+
+
+#Deliverable 3
+#t test 
+t.test(suspension_Coil$PSI,mu=1500) 
+
+#t test with subsets
+#lot 1
+t.test(subset(suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+#lot 3
+t.test(subset(suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+#lot 4
+t.test(subset(suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
